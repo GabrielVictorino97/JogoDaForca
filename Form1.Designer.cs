b1fe3e31a883eb71fe1f9cc1fc57f86480bdba13
Gabriel Victorino <55957207+gabrielvictorino1997@users.noreply.github.com>
2019-11-18 15:04:40 -0300

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,24 +30,29 @@
         {
             this.btnStart = new System.Windows.Forms.Button();
             this.pnForca = new System.Windows.Forms.Panel();
-            this.lbLetra = new System.Windows.Forms.Label();
-            this.txtLetra = new System.Windows.Forms.TextBox();
-            this.gbTentativas = new System.Windows.Forms.GroupBox();
-            this.label3 = new System.Windows.Forms.Label();
+            this.btnValidar = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.lbTrestantes = new System.Windows.Forms.Label();
-            this.btnValidar = new System.Windows.Forms.Button();
+            this.gbTentativas = new System.Windows.Forms.GroupBox();
+            this.lbTotalTentativas = new System.Windows.Forms.Label();
+            this.txtLetra = new System.Windows.Forms.TextBox();
+            this.lbLetra = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPalavra = new System.Windows.Forms.TextBox();
+            this.gbDica = new System.Windows.Forms.GroupBox();
+            this.lbDica = new System.Windows.Forms.Label();
             this.pnForca.SuspendLayout();
-            this.gbTentativas.SuspendLayout();
             this.groupBox1.SuspendLayout();
+            this.gbTentativas.SuspendLayout();
+            this.gbDica.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnStart
             // 
             this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnStart.Location = new System.Drawing.Point(17, 448);
+            this.btnStart.Location = new System.Drawing.Point(17, 445);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(113, 47);
+            this.btnStart.Size = new System.Drawing.Size(771, 42);
             this.btnStart.TabIndex = 1;
             this.btnStart.Text = "Start";
             this.btnStart.UseMnemonic = false;
@@ -56,6 +61,9 @@
             // 
             // pnForca
             // 
+            this.pnForca.Controls.Add(this.gbDica);
+            this.pnForca.Controls.Add(this.txtPalavra);
+            this.pnForca.Controls.Add(this.label1);
             this.pnForca.Controls.Add(this.btnValidar);
             this.pnForca.Controls.Add(this.groupBox1);
             this.pnForca.Controls.Add(this.gbTentativas);
@@ -67,51 +75,22 @@
             this.pnForca.TabIndex = 2;
             this.pnForca.Visible = false;
             // 
-            // lbLetra
+            // btnValidar
             // 
-            this.lbLetra.AutoSize = true;
-            this.lbLetra.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbLetra.Location = new System.Drawing.Point(3, 375);
-            this.lbLetra.Name = "lbLetra";
-            this.lbLetra.Size = new System.Drawing.Size(162, 25);
-            this.lbLetra.TabIndex = 0;
-            this.lbLetra.Text = "Digite uma letra";
-            this.lbLetra.UseMnemonic = false;
-            // 
-            // txtLetra
-            // 
-            this.txtLetra.Location = new System.Drawing.Point(3, 403);
-            this.txtLetra.MaxLength = 1;
-            this.txtLetra.Name = "txtLetra";
-            this.txtLetra.Size = new System.Drawing.Size(162, 20);
-            this.txtLetra.TabIndex = 1;
-            // 
-            // gbTentativas
-            // 
-            this.gbTentativas.Controls.Add(this.label3);
-            this.gbTentativas.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.gbTentativas.Location = new System.Drawing.Point(527, 57);
-            this.gbTentativas.Name = "gbTentativas";
-            this.gbTentativas.Size = new System.Drawing.Size(241, 132);
-            this.gbTentativas.TabIndex = 2;
-            this.gbTentativas.TabStop = false;
-            this.gbTentativas.Text = "Total de tentativas";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(85, 51);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(37, 39);
-            this.label3.TabIndex = 0;
-            this.label3.Text = "5";
+            this.btnValidar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnValidar.Location = new System.Drawing.Point(171, 375);
+            this.btnValidar.Name = "btnValidar";
+            this.btnValidar.Size = new System.Drawing.Size(75, 48);
+            this.btnValidar.TabIndex = 4;
+            this.btnValidar.Text = "Validar";
+            this.btnValidar.UseVisualStyleBackColor = true;
+            this.btnValidar.Click += new System.EventHandler(this.btnValidar_Click);
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.lbTrestantes);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(527, 195);
+            this.groupBox1.Location = new System.Drawing.Point(527, 148);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(241, 132);
             this.groupBox1.TabIndex = 3;
@@ -128,31 +107,102 @@
             this.lbTrestantes.TabIndex = 0;
             this.lbTrestantes.Text = "5";
             // 
-            // btnValidar
+            // gbTentativas
             // 
-            this.btnValidar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnValidar.Location = new System.Drawing.Point(171, 375);
-            this.btnValidar.Name = "btnValidar";
-            this.btnValidar.Size = new System.Drawing.Size(75, 48);
-            this.btnValidar.TabIndex = 4;
-            this.btnValidar.Text = "Validar";
-            this.btnValidar.UseVisualStyleBackColor = true;
+            this.gbTentativas.Controls.Add(this.lbTotalTentativas);
+            this.gbTentativas.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbTentativas.Location = new System.Drawing.Point(527, 10);
+            this.gbTentativas.Name = "gbTentativas";
+            this.gbTentativas.Size = new System.Drawing.Size(241, 132);
+            this.gbTentativas.TabIndex = 2;
+            this.gbTentativas.TabStop = false;
+            this.gbTentativas.Text = "Total de tentativas";
+            // 
+            // lbTotalTentativas
+            // 
+            this.lbTotalTentativas.AutoSize = true;
+            this.lbTotalTentativas.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTotalTentativas.Location = new System.Drawing.Point(85, 51);
+            this.lbTotalTentativas.Name = "lbTotalTentativas";
+            this.lbTotalTentativas.Size = new System.Drawing.Size(37, 39);
+            this.lbTotalTentativas.TabIndex = 0;
+            this.lbTotalTentativas.Text = "5";
+            // 
+            // txtLetra
+            // 
+            this.txtLetra.Location = new System.Drawing.Point(3, 403);
+            this.txtLetra.MaxLength = 1;
+            this.txtLetra.Name = "txtLetra";
+            this.txtLetra.Size = new System.Drawing.Size(162, 20);
+            this.txtLetra.TabIndex = 1;
+            // 
+            // lbLetra
+            // 
+            this.lbLetra.AutoSize = true;
+            this.lbLetra.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbLetra.Location = new System.Drawing.Point(3, 375);
+            this.lbLetra.Name = "lbLetra";
+            this.lbLetra.Size = new System.Drawing.Size(162, 25);
+            this.lbLetra.TabIndex = 0;
+            this.lbLetra.Text = "Digite uma letra";
+            this.lbLetra.UseMnemonic = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(5, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(282, 31);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Identifique a Palavra";
+            // 
+            // txtPalavra
+            // 
+            this.txtPalavra.Location = new System.Drawing.Point(11, 45);
+            this.txtPalavra.Name = "txtPalavra";
+            this.txtPalavra.Size = new System.Drawing.Size(276, 20);
+            this.txtPalavra.TabIndex = 6;
+            this.txtPalavra.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // gbDica
+            // 
+            this.gbDica.Controls.Add(this.lbDica);
+            this.gbDica.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbDica.Location = new System.Drawing.Point(263, 291);
+            this.gbDica.Name = "gbDica";
+            this.gbDica.Size = new System.Drawing.Size(505, 132);
+            this.gbDica.TabIndex = 7;
+            this.gbDica.TabStop = false;
+            this.gbDica.Text = "Dica";
+            // 
+            // lbDica
+            // 
+            this.lbDica.AutoSize = true;
+            this.lbDica.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDica.Location = new System.Drawing.Point(6, 26);
+            this.lbDica.Name = "lbDica";
+            this.lbDica.Size = new System.Drawing.Size(45, 20);
+            this.lbDica.TabIndex = 0;
+            this.lbDica.Text = "Dica";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(811, 541);
+            this.ClientSize = new System.Drawing.Size(811, 489);
             this.Controls.Add(this.pnForca);
             this.Controls.Add(this.btnStart);
             this.Name = "Form1";
             this.Text = "Jogo da Forca";
             this.pnForca.ResumeLayout(false);
             this.pnForca.PerformLayout();
-            this.gbTentativas.ResumeLayout(false);
-            this.gbTentativas.PerformLayout();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.gbTentativas.ResumeLayout(false);
+            this.gbTentativas.PerformLayout();
+            this.gbDica.ResumeLayout(false);
+            this.gbDica.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -163,10 +213,14 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label lbTrestantes;
         private System.Windows.Forms.GroupBox gbTentativas;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbTotalTentativas;
         private System.Windows.Forms.TextBox txtLetra;
         private System.Windows.Forms.Label lbLetra;
         private System.Windows.Forms.Button btnValidar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox gbDica;
+        private System.Windows.Forms.Label lbDica;
+        private System.Windows.Forms.TextBox txtPalavra;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,21 +12,165 @@ namespace Jogo_da_Forca
 {
     public partial class Form1 : Form
     {
+        private String palavra;
+        private String tela;
+        private int tentativas;
+        private int pospalavra;
+        private String[] perguntas;
+        private String[] dicas;
+
         public Form1()
         {
             InitializeComponent();
+            this.EscolhePerguntas();
         }
 
-        private void EscolhePalava()
+        public void EscolhePerguntas()
         {
+            perguntas = new String[25];
+            dicas = new String[25];
 
+
+            #region perguntasEdicas
+
+            perguntas[0] = "biblioteca";
+            dicas[0] = "Acredite se quiser, tem na sua escola";
+
+            perguntas[1] = "estrada";
+            dicas[1] = "Passa carros";
+
+            perguntas[2] = "aviao";
+            dicas[2] = "Não é um passaro nem o super homem";
+
+            perguntas[3] = "bandeira";
+            dicas[3] = "Quem fez do japão tava com preguiça";
+
+            perguntas[4] = "bigode";
+            dicas[4] = "Nem todo homem tem, mas algumas meninas tem";
+
+            perguntas[5] = "bola";
+            dicas[5] = "Chutar";
+
+            perguntas[6] = "dentista";
+            dicas[6] = "Usa a maquininha do capeta";
+
+            perguntas[7] = "fazenda";
+            dicas[7] = "Tem animais";
+
+            perguntas[8] = "galinha";
+            dicas[8] = "Ave mas não voa";
+
+            perguntas[9] = "gato";
+            dicas[9] = "Cachorro é melhor";
+
+            perguntas[10] = "laranja";
+            dicas[10] = "Fruta ";
+
+            perguntas[11] = "mapa";
+            dicas[11] = "Define o caminho";
+
+            perguntas[12] = "martelo";
+            dicas[12] = "Thor";
+
+            perguntas[13] = "nariz";
+            dicas[13] = "Serve para sentir cheiros";
+
+            perguntas[14] = "ovo";
+            dicas[14] = "quem nasceu primeiro?";
+
+            perguntas[15] = "palhaço";
+            dicas[15] = "Amigo que não ajuda no trabalho";
+
+            perguntas[16] = "pirata";
+            dicas[16] = "jack sparrow";
+
+            perguntas[17] = "palmeiras";
+            dicas[17] = "Não tem mundial";
+
+            perguntas[18] = "heroi";
+            dicas[18] = "Salva as pessoas";
+
+            perguntas[19] = "mulher";
+            dicas[19] = "Sempre tem razão";
+
+            perguntas[20] = "paraguai";
+            dicas[20] = "Falsificado";
+
+            perguntas[21] = "jogos";
+            dicas[21] = "Sai mais estressado do que quando começou";
+
+            perguntas[22] = "computador";
+            dicas[22] = "Melhor invenção da tecnologia";
+
+            perguntas[23] = "internet";
+            dicas[23] = "Ninguém vive sem";
+
+            perguntas[24] = "animal";
+            dicas[24] = "Pode ser de estimação";
+            #endregion
+
+
+        }
+
+        private void IniciaJogo()
+        {
+            Random r = new Random();
+            pospalavra = r.Next(0, 25);
+            palavra = perguntas[pospalavra];
+            tela = "";
+            
+            for (int i = 0; i < palavra.Length; i++)
+            {
+                tela = tela + "#";
+            }
+
+            txtPalavra.Text = tela;
+            lbDica.Text = dicas[pospalavra];
+            tentativas = 5;
+            lbTrestantes.Text = tentativas.ToString();
+            lbTotalTentativas.Text = tentativas.ToString();
         }
 
         private void btnStart_Click(object sender, EventArgs e)
         {
             pnForca.Visible = true;
             btnStart.Visible = false;
+            this.IniciaJogo();
 
+        }
+
+        private void btnValidar_Click(object sender, EventArgs e)
+        {
+
+           
+                Boolean encontrouLetra = false;
+                Char letra = Convert.ToChar(txtLetra.Text);
+               // int pLetra = 0;
+                string txt = "";
+
+                for (int i = 0; i < palavra.Length; i++)
+                {
+                    
+                    if (palavra[i] == letra)
+                    {
+                        encontrouLetra = true;
+                        //pLetra = i;
+                        txt = txt + txtLetra.Text;
+                    }
+                    else
+                    {
+                        txt = txt + tela[i];
+                    }
+                }
+                tela = txt;
+                txtPalavra.Text = tela;
+                txtPalavra.Clear();
+                tentativas--;
+            if (tentativas <= 0)
+            {
+                pnForca.Visible = false;
+
+            }
         }
     }
 }
